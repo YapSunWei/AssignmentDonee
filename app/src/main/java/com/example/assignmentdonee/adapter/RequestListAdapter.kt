8d1b86608c5300e12ab39bf3087c7c6c0055b10a
SeanYap <yapsw0904@gmail.com>
2022-09-26 22:16:50 +0800

    commit 20220926 - Modified Files Location

--- app/src/main/java/com/example/assignmentdonee/adapter/RequestListAdapter.kt
+++ app/src/main/java/com/example/assignmentdonee/adapter/RequestListAdapter.kt
@@ -1,4 +1,60 @@
 package com.example.assignmentdonee.adapter
 
-class RequestListAdapter {
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+import java.util.ArrayList
+
+import com.example.assignmentdonee.R
+
+import com.example.assignmentdonee.model.Requests
+
+class RequestListAdapter(private var activity: Activity, private var items: ArrayList<Requests>): BaseAdapter()  {
+
+    private class ViewHolder(row: View?) {
+        var txtTitle: TextView? = null
+
+        init {
+            this.txtTitle = row?.findViewById(R.id.textModelo)
+        }
+    }
+
+    @SuppressLint("InflateParams")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view: View?
+        val viewHolder: ViewHolder
+        if (convertView == null) {
+            val inflater = activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            view = inflater.inflate(R.layout.request_list, null)
+            viewHolder = ViewHolder(view)
+            view?.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+
+        val request = items[position]
+        viewHolder.txtTitle?.text = request.requestID
+
+        return view as View
+    }
+
+    override fun getItem(i: Int): Requests {
+        return items[i]
+    }
+
+    override fun getItemId(i: Int): Long {
+        return i.toLong()
+    }
+
+    override fun getCount(): Int {
+        return items.size
+    }
 }
