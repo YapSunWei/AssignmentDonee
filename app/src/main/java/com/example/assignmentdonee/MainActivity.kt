7a03cdd25398192e9e785151277e11d5bdbb9f5a
SeanYap <yapsw0904@gmail.com>
2022-09-26 14:09:14 +0800

    commit 20220926 - EditForm added

--- app/src/main/java/com/example/assignmentdonee/MainActivity.kt
+++ app/src/main/java/com/example/assignmentdonee/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.assignmentdonee
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       val button1 : Button = findViewById(R.id.requestBtn)
+       val button2 : Button = findViewById(R.id.editBtn)
+
+        button1.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, RequestItem::class.java)
+            startActivity(intent)
+        })
+
+        button2.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, EditItem::class.java)
+            startActivity(intent)
+        })
+
+
+
     }
+
 }
